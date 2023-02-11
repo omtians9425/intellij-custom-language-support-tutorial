07e6e8400277f352acdbd3eb529b884891dfdf0a
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2023-02-11 10:57:31 +0900

    Create SimpleIcons

---
+++ src/main/kotlin/com/github/omtians9425/intellijcustomlanguagesupporttutorial/language/SimpleIcons.kt
@@ -0,0 +1,9 @@
+package com.github.omtians9425.intellijcustomlanguagesupporttutorial.language
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+
+object SimpleIcons {
+    val FILE: Icon = IconLoader.getIcon("/icons/jar-gray.png", SimpleIcons::class.java)
+}
